34164b10b946f62ae0ede097e8a0228d63ea8ce8
Orianamartina <orianapettinelli04@gmail.com>
2023-06-26 12:03:52 -0300

    error boundary needs research

--- space/src/components/landing/Landing.jsx
+++ space/src/components/landing/Landing.jsx
@@ -8,29 +8,6 @@ export default function Landing () {
 
 
 
-    return (
-
-        <div>
-            <h1>wenas</h1>
-            <div className="divTable">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Nombre</th>
-                            <th>Sueldo</th>
-                            <th>Provincia</th>
-                            <th>Status</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {/* Renderizar Datos */}
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    )
-
 
 
 
