893ae7ac5b6c4f03d84586b2d8655f3ce6592629
Orianamartina <orianapettinelli04@gmail.com>
2023-06-26 21:02:50 -0300

    loading div when loading img

--- space/src/components/PictureOfDay/PictureOfDay.jsx
+++ space/src/components/PictureOfDay/PictureOfDay.jsx
@@ -19,11 +19,22 @@ const PictureOfDay = () => {
         
     }, [dispatch])
     return(
+        
         <div>
-            <h1>{pic.date}</h1>
-            <h1>{pic.title}</h1>
-            <img src={pic.url} alt="" />
-            <p>{pic.explanation}</p>
+            {pic?(
+                <div>
+                    <h1>{pic.date}</h1>
+                    <h1>{pic.title}</h1>
+                    <img src={pic.url} alt="" />
+                    <h3>Copyright: {pic.copyright}</h3>
+                    <p>{pic.explanation}</p> 
+                </div>
+            ):(
+                <div>
+                    Loading...
+                </div>
+            )}
+            
         </div>
     )
 }
